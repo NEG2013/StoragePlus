1bf96f5e3579682398083963014baad012f7c6f3
NEG2013 <NEG98765@Gmail.com>
2016-08-17 20:05:08 -0600

    1.2.01b

--- src/main/java/com/drunkripper/storageplus/item/StorageCell.java
+++ src/main/java/com/drunkripper/storageplus/item/StorageCell.java
@@ -2,6 +2,13 @@ package com.drunkripper.storageplus.item;
 
 import net.minecraft.item.Item;
 
-public class StorageCell extends Item {
+public class StorageCell extends GItem {
+
+	public static int types;
+	
+	public StorageCell(int types, String name) {
+		super(name);
+		this.types = types;
+	}
 
 }
--- src/main/java/com/drunkripper/storageplus/registry/ItemRegsitry.java
+++ src/main/java/com/drunkripper/storageplus/registry/ItemRegsitry.java
@@ -4,6 +4,7 @@ import com.drunkripper.storageplus.block.Controller;
 import com.drunkripper.storageplus.block.GBlock;
 import com.drunkripper.storageplus.block.StorageNetwork;
 import com.drunkripper.storageplus.item.GItem;
+import com.drunkripper.storageplus.item.StorageCell;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
@@ -15,10 +16,10 @@ public class ItemRegsitry {
 
 	public static void initItems() {
 
-		storage_cell_basic = new GItem("storage_cell_basic");
-		storage_cell = new GItem("storage_cell");
-		storage_cell_advanced = new GItem("storage_cell_advanced");
-		storage_cell_ultimate = new GItem("storage_cell_ultimate");
+		storage_cell_basic = new StorageCell(8, "storage_cell_basic");
+		storage_cell = new StorageCell(32, "storage_cell");
+		storage_cell_advanced = new StorageCell(128, "storage_cell_advanced");
+		storage_cell_ultimate = new StorageCell(512, "storage_cell_ultimate");
 	}
 
 	public static void registerRenders() {
